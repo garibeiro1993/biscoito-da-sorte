7f1e0c0eb1b58b0675b9c008da0452ccac0d3ad9
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-08-20 19:10:56 -0300

    Create a App Biscoito da Sorte

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './estilo.css';
 
-export default function App() {
+const App = () => {
+
+  const [textoFrase, setTextoFrase] = useState('')
+
+  const frasesBiscoito = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.',
+    'O riso é a menor distância entre duas pessoas.',
+    'Deixe de lado as preocupações e seja feliz.',
+    'Realize o óbvio, pense no improvável e conquiste o impossível.',
+    'Acredite em milagres, mas não dependa deles.',
+    'A maior barreira para o sucesso é o medo do fracasso.'];
+
+  const quebraBiscoito = () => {
+    let numeroAleatorio = Math.floor(Math.random() * frasesBiscoito.length);
+    setTextoFrase('"' + frasesBiscoito[numeroAleatorio] + '"');
+  }
+
   return (
-    <div>
-      'oi'
+    <div className="container">
+      <img src={require('./assets/biscoito.png')} className="img" alt='biscoito da sorte' />
+      <Botao nome="Abrir Biscoito" acaoBtn={quebraBiscoito} />
+      <h3 className="textoFrase">{textoFrase}</h3>
     </div>
   )
 }
+
+const Botao = (props) => {
+  return (
+    <div>
+      <button onClick={props.acaoBtn}>{props.nome}</button>
+    </div>
+  )
+}
+
+export default App;
